5c6fdcd917a9a5a5c21a594a1343e9a5111c246a
ayush110404 <ayushagarwal1102004@gmail.com>
2025-02-04 18:58:48 +0530

    improved code for bst implementation

--- DataStructureImplementation/prg6.cpp
+++ DataStructureImplementation/prg6.cpp
@@ -1,50 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class bst{
+class bst {
 public:
     int data;
     bst *right;
     bst *left;
-    bst(int x){
-        this->data=x;
-        this->left=this->right=NULL;
-    }
+    bst(int x) : data(x), left(nullptr), right(nullptr) {}
 };
 
-bst *insert(bst *root,int data){
-    if(!root){
-        bst *newnode = new bst(data);
-        return newnode;
-    }
-    if(data>root->data){
-        root->right=insert(root->right,data);
-    }
-    else{
-        root->left=insert(root->left,data);
-    }
+bst* insert(bst* root, int data) {
+    if (!root) return new bst(data);
+    if (data > root->data) root->right = insert(root->right, data);
+    else root->left = insert(root->left, data);
     return root;
 }
 
-void inorder(bst *node){
-    if(node==NULL) return;
-    else {
-        inorder(node->left);
-        cout<<node->data<<" ";
-        inorder(node->right);
-    }
+void inorder(bst* node) {
+    if (!node) return;
+    inorder(node->left);
+    cout << node->data << " ";
+    inorder(node->right);
 }
 
-int main(){
-    cout<<"Enter BST: "<<"\n";
-    bst *root=NULL;
-    root=insert(root,30);
-    root=insert(root,13);
-    root=insert(root,12);
-    root=insert(root,54);
-    root=insert(root,90);
-    root=insert(root,15);
+int main() {
+    bst* root = nullptr;
+    vector<int> values = {30, 13, 12, 54, 90, 15};
+    for (int val : values) root = insert(root, val);
     inorder(root);
-
-
 }
